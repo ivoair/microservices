43823628ee10ebfd01e13a0ba72b0f24e3718314
ivoair <ivancarro@gmail.com>
2016-06-14 11:55:44 +0200

    Spring Cloud Config Server and Zuul Proxy

--- EurekaServer/src/main/java/com/ms/EurekaServerApplication.java
+++ EurekaServer/src/main/java/com/ms/EurekaServerApplication.java
@@ -1,9 +1,13 @@
 package com.ms;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+@EnableAutoConfiguration
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaServerApplication {
@@ -11,4 +15,5 @@ public class EurekaServerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaServerApplication.class, args);
 	}
+	
 }
