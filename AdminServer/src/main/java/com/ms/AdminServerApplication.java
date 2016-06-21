ffd251a9d5ef92c334a510034a7b4b9ab54c41e1
ivoair <ivancarro@gmail.com>
2016-06-21 12:10:21 +0200

    Spring Boot Admin

--- AdminServer/src/main/java/com/ms/AdminServerApplication.java
+++ AdminServer/src/main/java/com/ms/AdminServerApplication.java
@@ -4,18 +4,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.context.annotation.Configuration;
+
+import de.codecentric.boot.admin.config.EnableAdminServer;
 
 @Configuration
 @EnableAutoConfiguration
 @SpringBootApplication
-@EnableEurekaServer
+@EnableAdminServer
 @EnableEurekaClient
-public class EurekaServerApplication {
+public class AdminServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EurekaServerApplication.class, args);
+		SpringApplication.run(AdminServerApplication.class, args);
 	}
-	
+
 }
